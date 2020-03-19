47deaad80fbfe5f2691044c67fa92ed4343063e3
Konstantin Burkalev <KSDaemon@ya.ru>
2020-03-19 21:17:46 +0200

    Build version

--- dist/nunjucks-intl-with-locales.js
+++ dist/nunjucks-intl-with-locales.js
@@ -15,7 +15,7 @@ var _utils = require("./utils.js");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var getNumberFormat = (0, _intlFormatCache["default"])(Intl.NumberFormat);
 var getDateTimeFormat = (0, _intlFormatCache["default"])(Intl.DateTimeFormat);
